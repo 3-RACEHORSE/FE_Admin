db0ccbd7dc0f6e1c635daf2b2a45228302523e84
YOON-CC <whdbscks77@gmail.com>
2024-06-12 04:39:15 +0900

    style: 불필요한 주석 제거 (#5)

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -3,20 +3,12 @@ import { ReactNode } from "react";
 type Props = {
   children: ReactNode;
   changeComponent: ReactNode;
-  post: ReactNode;
   postInfo: ReactNode;
 };
-export default function Layout({
-  children,
-  changeComponent,
-  post,
-  postInfo,
-}: Props) {
+export default function Layout({ children, changeComponent, postInfo }: Props) {
   return (
     <div>
       {children}
-      {post}
-
       <div
         className="flex items-center justify-between w-full mt-28"
         style={{ height: "350px", paddingLeft: "2.5%", paddingRight: "2.5%" }}
--- src/components/pages/Post.tsx
+++ src/components/pages/Post.tsx
@@ -1,30 +1,8 @@
 export default function Post() {
   return (
     <div
-      style={{
-        position: "fixed",
-        width: "100%",
-        height: "100%",
-        backgroundColor: "rgba(0, 0, 0, 0.001)",
-
-        zIndex: 1,
-      }}
-    >
-      {/* <div
-        style={{
-          position: "relative",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          backgroundColor: "white",
-          width: "80%",
-          height: "90%",
-          borderRadius: "10px",
-          paddingTop: "5%",
-        }}
-      >
-        안녕
-      </div> */}
-    </div>
+      className=" h-full rounded-lg bg-[#1e2938]"
+      style={{ width: "100%" }}
+    ></div>
   );
 }
