b8a2657d07d2cc8494c30c7b364b0146957b70f0
YOON-CC <whdbscks77@gmail.com>
2024-06-11 20:29:41 +0900

    feat: 페러렐을 위한, 버튼 레이아웃 구현 (#1)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="bg-[#1e293B] text-white">
       <div className="container mx-auto flex justify-between items-center p-4">
-        <div className="text-lg font-bold">MEET PLUS</div>
+        <div className="text-lg font-bold">ADMIN SYSTEM</div>
         <nav className="flex space-x-4">
           <div className="hover:underline">1</div>
           <div className="hover:underline">2</div>
