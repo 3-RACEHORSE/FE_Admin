b0571cd2f9fc52994d103cc8fdbd3b0e4c1f7b45
YOON-CC <whdbscks77@gmail.com>
2024-06-11 21:18:17 +0900

    feat: 아이콘 적용 및 텍스트 정렬 (#3)

--- src/components/view/StatusView.tsx
+++ src/components/view/StatusView.tsx
@@ -1,3 +1,5 @@
+import { BiSolidDonateHeart } from "react-icons/bi";
+
 export default function StatusView() {
   return (
     <div
@@ -5,10 +7,10 @@ export default function StatusView() {
       style={{ width: "30%" }}
     >
       <div className="flex justify-between font-bold text-gray-400 text-lg">
-        <p>Donation</p>
-        <p>Donation</p>
+        <p className="flex justify-center items-center">Donation</p>
+        <BiSolidDonateHeart size={40} />
       </div>
-      <div className="flex justify-between font-bold text-white-400 text-4xl">
+      <div className="flex justify-between font-bold text-white-400 text-3xl">
         <p>100,000,000</p>
       </div>
       <div className="flex justify-between font-bold text-white-400 text-sm bg-[#121822] rounded-lg">
